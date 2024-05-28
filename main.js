3b1f801087ee94b31c174c67f86acc1548307061
JMBeltranDev <jmbeltran.fsdeveloper@gmail.com>
2024-05-28 16:39:07 -0500

    update btn hover and JS

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 "use strict";
-import data from "./data.json" with { type: "json" };
 
 let containerItems = document.querySelector(".container-items");
 
-data.forEach((element,index) => {
-  
-  containerItems.innerHTML+=`
-  <div class="summary-item item-color--${index+1}">
+const getData = async function () {
+  let response = await fetch("./data.json");
+  let data = await response.json();
+  data.forEach((element, index) => {
+    containerItems.innerHTML += `
+  <div class="summary-item item-color--${index + 1}">
     <div class="item-box">
       <img src="${element.icon}" alt="icon" class="summary-item-icon">
       <h3 class="summary-item-title">${element.category}</h3>
     </div>
     <p class="summary-score"><span>${element.score}</span> / 100</p>
-  </div>`
-    
-});
+  </div>`;
+  });
+};
+getData();
